e7427b09eba49588aa4710f5b01a1993e2faa560
gaumeloth <guido.discacciati@gmail.com>
2023-05-01 23:17:17 +0200

    aggiunto plugin allo script di setup per ranger

--- scripts/rangerPluginsSetup.sh
+++ scripts/rangerPluginsSetup.sh
@@ -8,4 +8,5 @@ git clone https://github.com/yonghie/ranger-gitplug
 cd ranger-gitplug
 make install
 
-
+git clone https://github.com/SL-RU/ranger_udisk_menu ~/.config/ranger/plugins/ranger_udisk_menu
+echo "from plugins.ranger_udisk_menu.mounter import mount" >> $HOME/.config/ranger/commands.py

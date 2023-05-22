b5c3b157f11d250c36e80d546b3f169e38a22c1e
gaumeloth <guido.discacciati@gmail.com>
2023-05-22 23:11:37 +0200

    cambiati i menù di rofi

--- rofi/powermenu/type-4/powermenu.sh
+++ rofi/powermenu/type-4/powermenu.sh
@@ -11,7 +11,7 @@
 
 # Current Theme
 dir="$HOME/.config/rofi/powermenu/type-4"
-theme='style-5'
+theme='style-2'
 
 # CMDs
 uptime="`uptime -p | sed -e 's/up //g'`"
@@ -73,6 +73,8 @@ run_cmd() {
 				i3-msg exit
 			elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
 				qdbus org.kde.ksmserver /KSMServer logout 0 0 0
+			elif [[ "$DESKTOP_SESSION" == 'qtile' ]]; then
+				qtile cmd-obj -o cmd -f shutdown
 			fi
 		fi
 	else

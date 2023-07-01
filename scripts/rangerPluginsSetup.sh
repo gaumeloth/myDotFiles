db7bf17a12972bd3217e6d9c51fac7324b66acba
gaumeloth <guido.discacciati@gmail.com>
2023-07-02 01:43:09 +0200

    aggiunte configurazioni di sddm e vaiety ed eliminato un dotfile inutilizzato | rifinito lo script che setta i plugin di ranger | spostato .gitconfing per comodità futura

--- scripts/rangerPluginsSetup.sh
+++ scripts/rangerPluginsSetup.sh
@@ -7,6 +7,8 @@ echo "default_linemode devicons2" >> $HOME/.config/ranger/rc.conf
 git clone https://github.com/yonghie/ranger-gitplug
 cd ranger-gitplug
 make install
+cd ..
+rm -r ranger-gitplug
 
 git clone https://github.com/SL-RU/ranger_udisk_menu ~/.config/ranger/plugins/ranger_udisk_menu
 echo "from plugins.ranger_udisk_menu.mounter import mount" >> $HOME/.config/ranger/commands.py
